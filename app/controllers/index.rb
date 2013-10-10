ae5d3803003764f6ea57fae8946355ae86a45127
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-10-10 05:58:59 -0500

    Model and migration files created, yet to run.  Logic for counter in controller started, yet to complete.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,14 @@
 get '/' do
-  # Look in app/views/index.erb
+  # let user create new short URL, display a list of shortened URLs
   erb :index
+end
+
+post '/urls' do
+  # creates a new Url
+end
+
+# e.g., /q6bda
+get '/:short_url' do
+  Url.find_by_short_url(params[:short:url])
+  # redirect to appropriate "long" URL
 end
