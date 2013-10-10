73628708749557a9afd1200650c6accae464b355
Adigo Atabo <Adigo@Adigos-MacBook-Air.local>
2013-10-10 06:46:03 -0500

    Counter completed, awaiting testing.  Url regex validation added.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -5,10 +5,15 @@ end
 
 post '/urls' do
   # creates a new Url
+  # if url is invalid display helpful error message
+  # to the user and give them the opportunity to correct
+  # the error
 end
 
 # e.g., /q6bda
 get '/:short_url' do
-  Url.find_by_short_url(params[:short:url])
-  # redirect to appropriate "long" URL
+  # url = Url.find_by_short_url(params[:short:url])
+  # url.click_count += 1
+  # # redirect to appropriate "long" URL
+  # redirect to("#{url.long_url}")
 end
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,3 +1,11 @@
 class Url < ActiveRecord::Base
-  # Remember to create a migration!
+  validates_presence_of :long_url, :short_url, :click_count
+  validates :long_url, format: { with: /^https?:\/\//, message: "Not a valid Url" }
+  validates :short_url, format: { with: /^https?:\/\//, message: "Not a valid Url" }
+  before_save :generate_short_url
+
+protected
+  def generate_short_url
+    
+  end
 end
